eaeb4561a4bc43a59b6db823603e37ce2f94783f
Chris Fraser <mrfunnel@gmail.com>
2013-02-24 23:44:01 +0200

    Initial Commit

---
+++ Scripts/bootstrap.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    var deviceReady = false;
+    var jqmMobileInit = false;
+
+    var initApp = function () {
+        if ((deviceReady && jqmMobileInit) || (jqmMobileInit && !mobile_system)) {
+            startApp();
+        }
+    };
+
+    var onDeviceReady = function () {
+        deviceReady = true;
+        //alert('dev ready');
+        initApp();
+    };
+
+    var onMobileInit = function () {
+        $.support.cors = true;
+        $.mobile.allowCrossDomainPages = true;
+        jqmMobileInit = true;
+        //alert('jqm ready');
+        initApp();
+    };
+
+    $(document).bind('mobileinit', onMobileInit);
+    document.addEventListener("deviceready", onDeviceReady, false);
+})();
---
+++ Scripts/main.js
@@ -0,0 +1,27 @@
+﻿var application = {
+    init: function () {
+        $('#status').text('Device Ready! Go for it.');
+        $('#btnScan').on('click', scan());
+    },
+    scan: function() {
+        console.log('scanning');
+        try {
+            window.plugins.barcodeScanner.scan(function(args) {
+                console.log("Scanner result: \n" +
+                    "text: " + args.text + "\n" +
+                    "format: " + args.format + "\n" +
+                    "cancelled: " + args.cancelled + "\n");
+
+                //                if (args.format == "QR_CODE") {
+                //                    window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
+                //                }
+
+                $('#scanResults').html(args.toJSON());
+            }, function(error) {
+                alert("Scanning failed: " + error);
+            });
+        } catch (ex) {
+            alert(ex.message);
+        }
+    }
+};
