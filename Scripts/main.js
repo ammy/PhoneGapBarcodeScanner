1a63fab5caa6e8d82d3b5038456cf0a89552fd9b
Chris Fraser <mrfunnel@gmail.com>
2013-02-25 09:48:23 +0200

    Trying

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,28 +1,43 @@
 ﻿var application = {
     urlRegex: '/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/',
     init: function () {
-        $('#goToUrl').hide();
+        console.log("init");
         $('#status').text('Device Ready! Go for it.');
-        $('#btnScan').bind('tap', this.scan);
-        $('#goToUrl').bind('tap', this.goToUrl);
+        $('#btnScan').on('tap', this.scan);
+        $('#goToUrl').on('tap', this.goToUrl);
     },
     scan: function() {
         console.log('scanning');
-        $('#goToUrl').hide();
+        $('#goToUrl').buttonMarkup('disable');
         try {
-            window.plugins.barcodeScanner.scan(function(args) {
-                console.log("Scanner result: \n" +
-                    "text: " + args.text + "\n" +
-                    "format: " + args.format + "\n" +
-                    "cancelled: " + args.cancelled + "\n");
-                
-                //match a url
-                if (args.format == "QR_CODE" && args.text.match(urlRegex)) {
-                    $('#goToUrl').show();
-                }
-                alert(args.text);
-                $('#scanResults').text(args.text);
-            });
+            if (window.plugins && window.plugins.barcodeScanner) {
+                window.plugins.barcodeScanner.scan(function(result) {
+                    alert("We got a barcode\n" +
+                        "Result: " + result.text + "\n" +
+                        "Format: " + result.format + "\n" +
+                        "Cancelled: " + result.cancelled);
+                    $('#scanResults').text(result.text);
+                }, function(error) {
+                    alert("Scanning failed: " + error);
+                });
+            } else {
+                $('#goToUrl').buttonMarkup('enable');
+
+                $('#scanResults').text("No scanner plugin available");
+                //$('#home').trigger('refresh');
+            }
+//            window.plugins.barcodeScanner.scan(function(args) {
+//                console.log("Scanner result: \n" +
+//                    "text: " + args.text + "\n" +
+//                    "format: " + args.format + "\n" +
+//                    "cancelled: " + args.cancelled + "\n");
+//                
+//                //match a url
+//                if (args.format == "QR_CODE" && args.text.match(urlRegex)) {
+//                }
+//                alert(args.text);
+//                $('#scanResults').text(args.text);
+//            });
         } catch (ex) {
             alert(ex.message);
         }
