6d4fd0705f21457ee2366022ed17a9badd7fe306
Chris Fraser <mrfunnel@gmail.com>
2013-02-25 10:02:51 +0200

    Added scanning lock

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,44 +1,34 @@
 ﻿var application = {
     urlRegex: '/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/',
+    isScanning: false,
     init: function () {
         console.log("init");
         $('#status').text('Device Ready! Go for it.');
         $('#btnScan').on('tap', this.scan);
         $('#goToUrl').on('tap', this.goToUrl);
     },
-    scan: function() {
+    scan: function () {
+        var that = this;
         console.log('scanning');
         $('#goToUrl').buttonMarkup('disable');
         try {
-            if (window.plugins && window.plugins.barcodeScanner) {
+            if (window.plugins && window.plugins.barcodeScanner && !this.isScanning) {
+                that.isScanning = true;
                 window.plugins.barcodeScanner.scan(function(result) {
-                    alert("We got a barcode\n" +
-                        "Result: " + result.text + "\n" +
-                        "Format: " + result.format + "\n" +
-                        "Cancelled: " + result.cancelled);
                     $('#scanResults').text(result.text);
+                    //match a url
+                    if (result.format == "QR_CODE" && result.text.match(that.urlRegex)) {
+                        $('#goToUrl').buttonMarkup('enable');
+                    }
+                    that.isScanning = false;
                 }, function(error) {
-                    alert("Scanning failed: " + error);
+                    $('#scanResults').text("Scanning failed: " + error);
+                    that.isScanning = false;
                 });
             } else {
-                $('#goToUrl').buttonMarkup('enable');
-
                 $('#scanResults').text("No scanner plugin available");
-                //$('#home').trigger('refresh');
             }
-//            window.plugins.barcodeScanner.scan(function(args) {
-//                console.log("Scanner result: \n" +
-//                    "text: " + args.text + "\n" +
-//                    "format: " + args.format + "\n" +
-//                    "cancelled: " + args.cancelled + "\n");
-//                
-//                //match a url
-//                if (args.format == "QR_CODE" && args.text.match(urlRegex)) {
-//                }
-//                alert(args.text);
-//                $('#scanResults').text(args.text);
-//            });
-        } catch (ex) {
+        } catch(ex) {
             alert(ex.message);
         }
     },
